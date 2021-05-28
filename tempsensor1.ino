0055eafc461bd6a969d2c1117eed262937b6f2b1
RRothermel <79158870+RRothermel@users.noreply.github.com>
2021-05-28 11:26:33 -0400

    Update tempsensor1.ino

--- tempsensor1.ino
+++ tempsensor1.ino
@@ -1,8 +1,8 @@
 #include <dht.h>
 
-
+// assigning the pins to different segments in the single digit display
 #define dht_apin A0 // Analog Pin sensor is connected to
-#define segA 2//connecting segment A to PIN2
+#define segA 2// connecting segment A to PIN2
 #define segB 3// connecting segment B to PIN3
 #define segC 4// connecting segment C to PIN4
 #define segD 5// connecting segment D to PIN5
@@ -10,26 +10,26 @@
 #define segF 7// connecting segment F to PIN7
 #define segG 8// connecting segment G to PIN8
 
-int COUNT=0;//count integer for 0-9 increment
+int COUNT=0;// count integer for 0-9 increment
  
 dht DHT;
- 
+
 void setup(){
  
   Serial.begin(9600);
-  delay(500);//Delay to let system boot
+  delay(500);// Delay to let system boot
   Serial.println("DHT11 Humidity & temperature Sensor\n\n");
-  delay(1000);//Wait before accessing Sensor
+  delay(1000);
 
   for (int i=2;i<9;i++)
   {
     pinMode(i, OUTPUT);// taking all pins from 2-8 as output
   }
  
-}//end "setup()"
+}
  
 void loop(){
-  //Start of Program 
+  // Start of Program 
  
   DHT.read11(dht_apin);
   
@@ -44,6 +44,7 @@ void loop(){
   Serial.print(ftemp);
   Serial.println(" F ");
 
+ // if statements to set up the display of digits 4-9 and 1 to show the temperature in the tens place ("1" if it is over 99 degrees)
   if (ftemp >= 100)
   {
     digitalWrite(segB, HIGH);
@@ -111,7 +112,5 @@ void loop(){
   }
   
   delay(5000);//Wait 5 seconds before accessing sensor again.
- 
-  //Fastest should be once every two seconds.
 
-}// end loop(
+}
